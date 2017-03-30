23db4863be1846a52851e942e0ed05816f31e75d
Siddharth Doshi <sidoshi510@gmail.com>
2017-03-30 15:44:10 +0530

    fix warnings for unhandeled error

--- src/folderStructure.js
+++ src/folderStructure.js
@@ -6,21 +6,21 @@ const createHead = require('./createHead.js')
 
 let folderStructure = ({name,description}) => {
     mkdirp('src/js', () => {
-        fs.writeFile('src/js/index.js','')
+        fs.writeFile('src/js/index.js','', (err) => err && console.log(err))
         mkdirp('src/js/vendor')
     })
     mkdirp('src/sass', () => {
-        fs.writeFile('src/sass/main.sass','')
+        fs.writeFile('src/sass/main.sass','', (err) => err && console.log(err))
         mkdirp('src/sass/vendor')
     })
     mkdirp('src/pug', () => {
-        fs.writeFile('src/pug/index.pug',createPug(name))
-        fs.writeFile('src/pug/_head.pug',createHead(name,description))
+        fs.writeFile('src/pug/index.pug',createPug(name), (err) => err && console.log(err))
+        fs.writeFile('src/pug/_head.pug',createHead(name,description),(err) => err && console.log(err))
     })
     mkdirp('build', () => {
-        fs.writeFile('build/index.js','')
-        fs.writeFile('build/index.html','')
-        fs.writeFile('build/main.css','')
+        fs.writeFile('build/index.js','',(err) => err && console.log(err))
+        fs.writeFile('build/index.html','',(err) => err && console.log(err))
+        fs.writeFile('build/main.css','',(err) => err && console.log(err))
         mkdirp('build/assets')
     })
 
